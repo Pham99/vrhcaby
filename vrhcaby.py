ade9b6febfbdeaee8cc78482387d0267865c139f
Razeyer007 <114099677+Razeyer007@users.noreply.github.com>
2023-04-26 18:16:51 +0200

    a lot of work

--- vrhcaby.py
+++ vrhcaby.py
@@ -37,10 +37,11 @@ class HraciPole:
 class Vrhcaby:
 
     def __init__(self) -> None:
-        self.hrac1 = Hrac()
-        self.hrac2 = Hrac()
-        self.kostka = Kostka()
-        self.hracideska = [HraciPole() for i in range(24)]
+        self.hrac1 = Hrac("cerny")
+        self.hrac2 = Hrac("bily")
+        self.currentplayer = self.hrac1
+        self.kostka = Kostka(6)
+        self.hracideska = [HraciPole() for _ in range(24)]
         self.cil_bily = []
         self.cil_cerny = []
         self.bar_cerny = HraciPole()
@@ -74,7 +75,17 @@ class Vrhcaby:
         for _ in range(5):
             self.hracideska[5].push(Kamen("bily"))
 
+    def napln_desku_debug(self):
+        self.hracideska[0].push(Kamen("cerny"))
+        self.hracideska[7].push(Kamen("bily"))
+        self.hracideska[6].push(Kamen("bily"))
+        self.hracideska[5].push(Kamen("bily"))
+        self.hracideska[8].push(Kamen("bily"))
+
+
     def move_kamen(self, zacatek, konec):
+        if self.hracideska[konec].length() < 2:
+            self.bar_bily.push(self.hracideska[konec].pop())
         self.hracideska[konec].push(self.hracideska[zacatek].pop())
 
     def display_mozne_tahy(self, kroky):
@@ -84,22 +95,25 @@ class Vrhcaby:
                 continue
             elif pole.peek() == "cerny":
                 tahy[i] = []
-        for key in tahy.keys():
+        keys = list(tahy.keys()).copy()
+        for key in keys:
             for i in kroky:
                 destinace = key + i
-                if destinace < 23 and destinace > -1:
-                    if self.hracideska[destinace].peek() != "bily" and self.hracideska[destinace].length() < 2:
+                if destinace < 24 and destinace > -1:
+                    if self.hracideska[destinace].peek() != "bily" or self.hracideska[destinace].length() < 2:
                         tahy[key].append(key + i)
+            if tahy[key] == []:
+                tahy.pop(key, None)
         return tahy
 
-    def vypocti_mozne_kroky(self, kroky):
+    def vypocti_mozne_kroky(self, kostka):
+        kroky = kostka.copy()
         if len(kroky) > 2:
             kroky = list(itertools.accumulate(kroky))
-            return kroky
         elif len(kroky) == 2:
             kroky.append(kroky[0] + kroky[1])
         else:
-            return kroky
+            pass
         return kroky
 
     def __str__(self) -> str:
@@ -114,42 +128,51 @@ class Vrhcaby:
                 print("----------")
         print("cil cerny:" + str(self.cil_cerny))
         print("bar_bily: " + str(self.bar_bily))
-        print("bar_cerny: " + str(self.bar_cerny))
+        print("bar_cerny: " + str(self.bar_cerny) + "\n")
 
-    def tah(self, zacatek, konec, dice):
-     krok = konec - zacatek
-     if krok in dice:
-          self.move_kamen(zacatek, konec)
-          dice.remove(krok)
-     else:
-        suma_dice = sum(dice)
-        while suma_dice - krok != sum(dice):
-             temp = dice.pop()
-             self.move_kamen(zacatek, zacatek + temp)
-             zacatek += temp
-             dice.remove(krok)
-        return dice
+    def tah(self, zacatek, konec, kostka):
+        krok = konec - zacatek
+        if krok in kostka:
+            self.move_kamen(zacatek, konec)
+            kostka.remove(krok)
+        else:
+            suma_dice = sum(kostka)
+            while suma_dice - krok != sum(kostka):
+                temp = kostka.pop()
+                self.move_kamen(zacatek, zacatek + temp)
+                zacatek += temp
+        return kostka
 
+    def switch_players(self):
+        if self.currentplayer == self.hrac1:
+            self.currentplayer = self.hrac2
+        else:
+            self.currentplayer = self.hrac1
+    
     def play(self):
-        a = Vrhcaby()
-        a.napln_desku()
+        self.napln_desku_debug()
         while not self.gameover:
-            kolo = True
-            if kolo:
-                print("hraje cerny")
-                kostky = a.dvojkostka()
-                while len(kostky) > 0 and kostky != None:
-                    print(f"dostali jste: " + str(kostky))
-                    #if "cerny" in list(map(str, a.bar_cerny)): #needs work
-                        #tah z baru od 0
-                        #pass
-                    #else:
-                    print(a.display_mozne_tahy(a.vypocti_mozne_kroky(kostky)))
-                    vyber = self.hrac1.Play()
-                    a.move_kamen(int(vyber[0]), int(vyber[1]))
-                    a.render()
-                #kolo = False
-                self.gameover_check()
+            print(f"hraje: {self.currentplayer}")
+            kostky = self.dvojkostka()
+            while len(kostky) > 0 and kostky != None:
+                print(f"vase kostky: " + str(kostky))
+                #if "cerny" in list(map(str, a.bar_cerny)): #needs work
+                    #tah z baru od 0
+                    #pass
+                #else:
+                mozne_tahy = self.display_mozne_tahy(self.vypocti_mozne_kroky(kostky))
+                if mozne_tahy == {}:
+                    print("zadne mozne tahy")
+                    break
+                print(mozne_tahy)
+                vyber = self.currentplayer.Play()
+                if vyber[0] not in mozne_tahy.keys() or vyber[1] not in mozne_tahy[vyber[0]]:
+                    print("zadal jste spatne")
+                    continue
+                kostky = self.tah(vyber[0], vyber[1], kostky)
+                self.render()
+            self.switch_players()
+            self.gameover_check()
         self.eval_winner()
                 
     def eval_winner(self):
@@ -175,13 +198,16 @@ class Kamen:
     
 class Hrac:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, barva) -> None:
+        self.barva = barva
 
     def Play(self):
         vyber = input("zadejte prikaz: ").split(" ")
         print("plaaay")
-        return vyber
+        return list(map(int, vyber))
+    
+    def __str__(self) -> str:
+        return self.barva
 
 class Hrac_CPU():
 
@@ -192,13 +218,6 @@ class Hrac_CPU():
         print("beep boop")
         
 a = Vrhcaby()
-# a.napln_desku()
-# a.render()
-# dice = [4, 5]
-# print(a.display_mozne_tahy(a.vypocti_mozne_kroky(dice)))
-# dice = a.tah(0, 4, dice)
-# a.render()
-# print(dice)
 a.play()
 
 
